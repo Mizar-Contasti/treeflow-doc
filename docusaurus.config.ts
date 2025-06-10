0f9253791307ae3b5d35012239a3ade764a84ce4
Mizar Contasti <contasti.mizar@gmail.com>
2025-06-10 15:15:54 -0600

    configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,6 +62,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    docs: {
+      sidebar: {
+        autoCollapseCategories: true,
+      },
+    },
     navbar: {
       title: 'TreeFlow',
       logo: {
